eb33f6388a6e4141d3cd5a48116082cf2a5510ef
JoaoGH <gabrielhartmannqwert@gmail.com>
2025-03-25 09:58:20 -0300

    feat: add seção de projetos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Navbar} from "./components/Navbar/Navbar.jsx";
 import {Hero} from "./components/Hero/Hero.jsx";
 import {TechnicalProficiency} from "./components/TechnicalProficiency/TechnicalProficiency.jsx";
 import {About} from "./components/About/About.jsx";
+import {Projects} from "./components/Projects/Projects.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <TechnicalProficiency/>
       <About/>
+      <Projects/>
     </div>
   );
 }
