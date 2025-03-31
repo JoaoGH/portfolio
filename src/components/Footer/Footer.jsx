72026a3fa70f1ccd9d927197ad3e01cd473eb8a8
JoaoGH <gabrielhartmannqwert@gmail.com>
2025-03-31 14:31:26 -0300

    refactor: melhorar internacionalização da seção

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import {useTranslation} from "react-i18next";
 
 export const Footer = () => {
+    const { t } = useTranslation();
+
     return (
         <footer id="footer" className="bg-purple-100 text-xs text-secondary text-center p-5">
-            Lorem Ipsum
+
+            {/* Direitos e créditos */}
+            <div className="text-sm">
+                <p>© {new Date().getFullYear() + " " + t("footer.copyright")}</p>
+                <p className="mt-1">{t("footer.using")}</p>
+            </div>
         </footer>
     );
-}
+};
