7f9e9c6a23a1c5d497dde7bed6df2203cd97bbc5
JoaoGH <gabrielhartmannqwert@gmail.com>
2025-04-01 15:41:15 -0300

    refactor: reformular tamanho da seção

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,36 +6,35 @@ export const About = () => {
     const {t} = useTranslation();
 
     return (
-        <section id="about" className="container mx-auto px-8 py-16">
-            <h4 className="block lg:hidden w-[200px] section-title bg-gradient-primary text-left mb-16">
-                {t("about.title")}
-            </h4>
+        <section id="about" className="bg-background">
+            <div className="container mx-auto px-15 py-12 md:py-16">
+                <h4 className="section-title bg-gradient-primary text-left">
+                    {t("about.title")}
+                </h4>
 
-            <div className="flex flex-col lg:flex-row gap-16 items-start justify-between">
-                <div className="flex-1">
-                    <h4 className="hidden lg:block w-[200px] section-title bg-gradient-primary text-left">
-                        {t("about.title")}
-                    </h4>
-                    <p className="text-sm text-justify leading-6 whitespace-pre-line mt-4">
+                <div className="mt-4 md:mt-6">
+                    <p className="text-sm text-justify leading-6 whitespace-pre-line">
                         <Trans i18nKey="about.text" components={{1: <br/>}}/>
                     </p>
 
-                    <div className="flex gap-4 mt-6">
-                        {ABOUT_ME.links.map((item) => {
-                            return (
-                                <a
-                                    key={item.id}
-                                    className="cursor-pointer group"
-                                    href={item.link}
-                                    target="_blank"
-                                    aria-label={item.label}
-                                >
-                                    <item.icon className="text-2xl text-secondary transition-transform duration-300
+                    <div className="flex gap-4 mt-6 md:mt-8">
+                        <div className="flex gap-4 mt-6">
+                            {ABOUT_ME.links.map((item) => {
+                                return (
+                                    <a
+                                        key={item.id}
+                                        className="cursor-pointer group"
+                                        href={item.link}
+                                        target="_blank"
+                                        aria-label={item.label}
+                                    >
+                                        <item.icon className="text-2xl text-secondary transition-transform duration-300
                                             group-hover:rotate-12 group-hover:scale-110 group-hover:translate-y-[-2px]
                                             group-hover:text-primary"/>
-                                </a>
-                            );
-                        })}
+                                    </a>
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
